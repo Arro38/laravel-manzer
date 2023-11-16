abcf86ac2abd7b3c3a1137578004f7147d59b4f2
arro38 <etienne.vaytilingom.pro@gmail.com>
2023-11-16 14:07:49 +0400

    Add Meal model and migration with fillable and user relationship.

---
+++ app/Models/Meal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meal extends Model
+{
+    use HasFactory;
+    // Add the fillable property to allow mass assignment on the model
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+        'user_id'
+    ];
+
+    // Add the relationship between the meal and the user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
